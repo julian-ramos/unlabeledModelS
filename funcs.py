35ec5a07124da37860d198e36f52bba531afda6e
Julian Ramos -Desktop CMU <ingenia@andrew.cmu.edu>
2014-04-22 10:36:43 -0400

    added the labels parameter to all of the functions using f1_score, by doing this now the f1_score works even when only subsets of the labels appear only in one of the two vector inputs. Main is currently working an generates all of statistics like spearmans and f1_scores.

--- funcs.py
+++ funcs.py
@@ -11,17 +11,19 @@ from sklearn import linear_model,svm,tree
 from sklearn.linear_model import SGDClassifier
 from sklearn.metrics import f1_score
 from sklearn.metrics import confusion_matrix
-from sklearn.metrics import f1_score
 import matplotlib.pylab as plt
 from sklearn.metrics import classification_report
 
 
 
-def clfsEval(trainData,testData,valData,trainLabels,testLabels,valLabels):
+def clfsEval(trainData,testData,valData,trainLabels,testLabels,valLabels,uLabels):
     '''
-    This function takes the data sets and build several classifiers
+    This function takes the data sets and builds several classifiers
     then they are all evaluated
     '''
+#     uLabels=np.hstack((np.unique(trainLabels),np.unique(testLabels),np.unique(valLabels)))
+#     uLabels=np.unique(uLabels)
+    
     clf={'classifier':[],'pred_test':[],'f1_score_test':[],'pred_val':[],\
          'f1_score_val':[],'confusion_matrix_test':[],'confusion_matrix_val':[],\
          'report_test':[],'report_val':[]}
@@ -36,18 +38,18 @@ def clfsEval(trainData,testData,valData,trainLabels,testLabels,valLabels):
         tempClf=clf['classifier'][i]
         tempClf.fit(trainData, trainLabels)
         clf['pred_test'].append(tempClf.predict(testData))
-        clf['f1_score_test'].append(f1_score(testLabels,clf['pred_test'][i]))
+        clf['f1_score_test'].append(f1_score(testLabels,clf['pred_test'][i],labels=uLabels))
         clf['confusion_matrix_test'].append(confusion_matrix(testLabels,clf['pred_test'][i]))
         clf['report_test'].append(classification_report(testLabels,clf['pred_test'][i]))
         clf['pred_val'].append(tempClf.predict(valData))
-        clf['f1_score_val'].append(f1_score(valLabels,clf['pred_val'][i]))
+        clf['f1_score_val'].append(f1_score(valLabels,clf['pred_val'][i],labels=uLabels))
         clf['confusion_matrix_val'].append(confusion_matrix(valLabels,clf['pred_val'][i]))
         clf['report_val'].append(classification_report(valLabels,clf['pred_val'][i]))
 
     
     return clf
 
-def agreementRates(clf,valLabels,plot=False):
+def agreementRates(clf,valLabels,uLabels,plot=False):
     '''
     This function gets a classifiers dictionary generated from clfsEval
     here we are going to calculate the agreement of the classifiers and
@@ -84,7 +86,7 @@ def agreementRates(clf,valLabels,plot=False):
         
             
     
-    sAgmnt=np.sort(np.unique(agmnt))[::-1]
+    sAgmnt=np.sort(np.unique(agmnt))
     f1_scores=[]
     for i in sAgmnt:
         inds2=np.argwhere(agmnt>=i)
@@ -97,8 +99,11 @@ def agreementRates(clf,valLabels,plot=False):
         #to the one produced by the real labels
         
         for cnum in range(len(clf['pred_val'])):
-            temp.append(f1_score(valLabels[inds2],clf['pred_val'][cnum][inds2]))
-            print(f1_score(valLabels[inds2],clf['pred_val'][cnum][inds2]))
+            try:
+                temp.append(f1_score(valLabels[inds2],clf['pred_val'][cnum][inds2],labels=uLabels))
+            except:
+                print('here')
+            print(f1_score(valLabels[inds2],clf['pred_val'][cnum][inds2],labels=uLabels))
         f1_scores.append(temp)
     if plot==True:
         plt.plot(sAgmnt,f1_scores)
@@ -112,7 +117,7 @@ def agreementRates(clf,valLabels,plot=False):
     return agmntVal,agmnt
     
         
-def clfsVal(clfs,agmntLabels,agmntLevels,agmntLvl=1):
+def clfsVal(clfs,agmntLabels,agmntLevels,uLabels,agmntLvl=1):
     '''
     Take the classifiers and the already calculated outputs and measure their f1_score
     for the labels generated from the agreement step
@@ -120,7 +125,7 @@ def clfsVal(clfs,agmntLabels,agmntLevels,agmntLvl=1):
     clfs['f1_score_agmnt']=[]
     for i in range(len(clfs['pred_val'])):
         inds=np.argwhere(np.array(agmntLevels)>=agmntLvl)
-        agmntF1=f1_score(np.array(agmntLabels)[inds],np.array(clfs['pred_val'][i])[inds])
+        agmntF1=f1_score(np.array(agmntLabels)[inds],np.array(clfs['pred_val'][i])[inds],labels=uLabels)
         clfs['f1_score_agmnt'].append(agmntF1)
         
     
@@ -128,4 +133,9 @@ def clfsVal(clfs,agmntLabels,agmntLevels,agmntLvl=1):
     
 
         
+
+    
+    
+    
+    
     
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ Model selection using unlabeled data
 '''
 from scipy.stats import spearmanr
 import numpy as np
+import dataML_Prepro as dmlPre
 from sklearn.datasets import  load_boston, load_iris, load_diabetes, load_digits, load_linnerud
 from sklearn.naive_bayes import GaussianNB
 from sklearn import linear_model
@@ -12,53 +13,53 @@ from sklearn.metrics import f1_score
 import funcs as fun
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
+import General as gF
 
-spear=[]
+
+#Datasets
+
+expe=gF.experimentVariables('unlabeledModelS')
+exec(expe['variables'])
+
+data=expe['data']
+labels=expe['labels']
 cont=0
-while cont<20:
+
+
+while cont<expe['numTests']:
     
     try:
-        datasets={'boston':load_boston(),'iris':load_iris(),'diabetes':load_diabetes(),'digits':load_digits(),'linnerud':load_linnerud()}
-        
-        data=datasets['iris']['data']#[0:135,:]
-        labels=datasets['iris']['target']#[0:135]
-        
         #Divide the data in training, testing and validation
+
+        splitData=dmlPre.dataSplitBalancedClass(data, labels)
+        trainData=splitData['trainData']
+        trainLabels=splitData['trainLabels']
+        testData=splitData['testData']
+        testLabels=splitData['testLabels']
+        valData=splitData['valData']
+        valLabels=splitData['valLabels']
+        uLabels=np.unique(np.hstack((np.unique(trainLabels),np.unique(testLabels),np.unique(valLabels))))
         
-        allInds=np.arange(len(data))
-        np.random.shuffle(allInds)
+        clfs=fun.clfsEval(trainData,testData,valData,trainLabels,testLabels,valLabels,uLabels)
+        agmntLabels,agmntLevels=fun.agreementRates(clfs,valLabels,uLabels,plot=True)
+        fun.clfsVal(clfs,agmntLabels,agmntLevels,uLabels,agmntLvl=expe['agmntlvl'])
+        f1_score_agmnt=f1_score(valLabels,agmntLabels,labels=uLabels)
+        confusion_agmnt=confusion_matrix(valLabels,agmntLabels)
         
-        chunkS=len(data)/3
-        trainInds=allInds[0:chunkS]
-        testInds=allInds[chunkS:chunkS*2]
-        valInds=allInds[chunkS*2:]
-        
-        trainData=data[trainInds]
-        trainTarget=labels[trainInds]
-        testData=data[testInds]
-        testTarget=labels[testInds]
-        valData=data[valInds]
-        valTarget=labels[valInds]
-        agmntlvl=0
-        clfs=fun.clfsEval(trainData,testData,valData,trainTarget,testTarget,valTarget)
-        agmntLabels,agmntLevels=fun.agreementRates(clfs,valTarget,plot=True)
-        fun.clfsVal(clfs,agmntLabels,agmntLevels,agmntLvl=agmntlvl)
-        f1_score_agmnt=f1_score(valTarget,agmntLabels)
-        confusion_agmnt=confusion_matrix(valTarget,agmntLabels)
-        
-        print('Agreement level used for filtering = %d'%(agmntlvl))
-        print('summary of results validation data')
-        print(clfs['report_val'][0])
-        
-        print('summary of results agreement data')
-        print(classification_report(valTarget,agmntLabels))
-        
-        print('f1 score test data',clfs['f1_score_test'])
-        print('f1 score validation data',clfs['f1_score_val'])
-        print('f1 score agmnt data',clfs['f1_score_agmnt'])
-        # print(clfs['confusion_matrix_val'],'confusion matrix val')
-        # print(confusion_agmnt,'confusion matrix agmnt')
-        print('f1_score validation and agmntLabels',f1_score_agmnt)
+        if expe['verbose']==1:
+            print('Agreement level used for filtering = %d'%(expe['agmntlvl']))
+            print('summary of results validation data')
+            print(clfs['report_val'][0])
+            
+            print('summary of results agreement data')
+            print(classification_report(valLabels,agmntLabels))
+            
+            print('f1 score test data',clfs['f1_score_test'])
+            print('f1 score validation data',clfs['f1_score_val'])
+            print('f1 score agmnt data',clfs['f1_score_agmnt'])
+            # print(clfs['confusion_matrix_val'],'confusion matrix val')
+            # print(confusion_agmnt,'confusion matrix agmnt')
+            print('f1_score validation and agmntLabels',f1_score_agmnt)
         
         
         #In this section I have to consider when there are 
@@ -68,66 +69,29 @@ while cont<20:
         
         rankVal=range(len(rVal))
         rankAgmnt=[int(np.argwhere(rVal==i)) for i in rAgmnt]
-        
-        print('models ordered by f1_score')
-        print(rVal)
-        print(rAgmnt)
-        
+
         rankVal=np.array(rankVal)+1
         rankAgmnt=np.array(rankAgmnt)+1
-        
-        print('models ranking')
-        print(rankVal)
-        print(rankAgmnt)
         spearmans=spearmanr(rankVal,rankAgmnt)
-        print(spearmans,'spearman rank correlation coefficient' )
+        
+        if expe['verbose']==1:
+            print('models ordered by f1_score')
+            print(rVal)
+            print(rAgmnt)
+            print('models ranking')
+            print(rankVal)
+            print(rankAgmnt)
+            print(spearmans,'spearman rank correlation coefficient' )
+        
+        
         spear.append(spearmans)
         cont+=1
     except:
         print('Failed trying again')
-
 print spear
 
-# Now I can rank the classifiers using the validation data and validation results
-# and compare that ranking to the one generated with the agmntLabels with those two
-# ranks I can use the spearman's ranking to compute whether the two rankings are
-# correlated or not I could also use the kappa's statistic to measure this
 
-
-
-# print(clfs)
-# #Naive Bayes model
-# gnb=GaussianNB()
-# y_gnb = gnb.fit(trainData, trainTarget).predict(testData)
-# #Linear regression
-# lgr = linear_model.LogisticRegression()
-# y_lgr = lgr.fit(trainData,trainTarget).predict(testData)
-#  
-# acuGnb=(testTarget == y_gnb).sum()/float(len(testTarget))
-# print("Accuracy Naive Bayes on test data : %f" % acuGnb)
-# f1Gnb=f1_score(testTarget,y_gnb)
-# print("f1_score Naive Bayes on test data : %f" % f1Gnb)
-# 
-# acuLgr=(testTarget == y_lgr).sum()/float(len(testTarget))
-# f1Lgr=f1_score(testTarget,y_gnb)
-# print("Accuracy Logistic regression on test data : %f" % acuLgr)
-# print("f1_score Logistic regression on test data : %f" % f1Lgr)
-# 
-# 
-# 
-# #Here comes the evaluation of the models with unlabeled data
-# val_gnb=gnb.predict(valData)
-# val_lgr=lgr.predict(valData)
-# 
-# ag=val_gnb==val_lgr
-# 
-# 
-# indsAg=[i for i in range(len(ag)) if ag[i]==True]
-# val_ag=val_gnb[indsAg]
-# 
-# print('accuracy on the agreed data',sum(val_ag==valTarget[indsAg])/float(len(val_ag)))
-# print('f1_score on the agreed data',f1_score(val_ag,valTarget[indsAg]))
-# print('data points used ',len(val_ag),' out of ',len(ag))
-
-
-
+# It seems like everything is working fine, what I need to do next is:
+# Calculate for everyrun the spearmans for different levels of agmnt
+# acumulate those results and then plot them with error whiskers, create a function to 
+# create that graph
