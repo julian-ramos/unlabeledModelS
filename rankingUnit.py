0e8af008e6492b539e25f8e70f71364b7a382c02
Julian Ramos -Desktop CMU <ingenia@andrew.cmu.edu>
2014-05-22 19:05:00 -0400

    Currently the ranking Unit already performs the experiment and generates results. Things left to do are: take out location features leave only the features used at the ubicomp paper fix the rank ties problem

--- rankingUnit.py
+++ rankingUnit.py
@@ -1,3 +1,4 @@
+import matplotlib.pylab as plt
 from sklearn.metrics import f1_score
 import dataSearchUtils as dSu
 import funcs as fun
@@ -12,6 +13,7 @@ intermediatePath='F:/Activity_RS/Last data set/FeatureDataset/intermediateData'
 modelsPath='F:/Activity_RS/Last data set/FeatureDataset/models'
 dataPath='F:/Activity_RS/Last data set/FeatureDataset/modelSelectionProjectData'
 filesList=os.listdir(dataPath)
+spearsa=[]
 
 filename=intermediatePath+'/'+'summary.plk'
 file=open(filename,'rb')
@@ -20,8 +22,38 @@ print('summary data loaded')
 agmnt=summary['agmnt']
 mv=summary['mv']
 votes=summary['votes']
+labels=summary['labels']
 
-ranks=fun.ranking(agmnt,mv,votes)
+agmntLvls=[np.unique(i).tolist() for i in agmnt]
+
+agmntLvls=[ np.min(i) for i in agmntLvls]
+agmntLvls=np.arange(np.max(agmntLvls),1,0.1)
+
+# agmntLvls=[ np.max(i) for i in agmntLvls]
+# agmntLvls=np.arange(np.max(agmntLvls),0.9,0.1)
+
+
+for agmntVal in agmntLvls: 
+    ranksMv,realRanks,spears=fun.ranking(agmnt,mv,votes,labels,agmntLvl=agmntVal)
+    spearsa.append(np.array(spears)[:,0])
+    print(np.mean(np.array(spears),0))
+    print(np.median(np.array(spears),0))
+    print(np.std(np.array(spears),0))
+    
+s=plt.boxplot(spearsa)
+#     plt.legend(s,['%d'%(agmntVal)])
+plt.show()
+
+# Currently there is a problem with the ranking procedure which is 
+# that I'm solving the ties by drawing at random however I shouldn't do that
+# instead the mean rank across the ones that are tied is calculated
+# I can probably solve this issue by extending the ranker function
+# to output two values instead of one. The second value is the rank while
+# the first value is the model number itself ordered by rank
+# This I believe penalizes less harshly the ranking for ties
+
+
+print('done')
 
 #Now that the ranks are ready I can compare them with the ranks
 #I get using the real labels from the data
